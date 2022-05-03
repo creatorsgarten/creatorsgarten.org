f2ac3ddb7bfe435bc2c5b27c55d7eb622e9f0418
Phoomparin Mano <poom@hey.com>
2022-05-03 22:55:38 +0700

    feat: thanks GPT-3

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,6 +18,18 @@
 	<section>
 		<p class="text-gray-800 max-w-2xl">The garden; the sanctuary for those who create.</p>
 	</section>
+
+	<section>
+		<p class="text-gray-800 max-w-2xl">
+			Creatorsgarten is a group for people who want to create things. We believe in the power of
+			creativity to change the world. We believe that everyone has the potential to be creative and
+			that creativity should be nurtured and celebrated. We believe in the power of collaboration
+			and the importance of sharing our ideas and creations with others. We believe in the
+			importance of taking risks, of experimentation, and of always learning and growing. We are a
+			community of creators who are passionate about making things and making a difference. Join us
+			and let's create something amazing together!
+		</p>
+	</section>
 </main>
 
 <style></style>
