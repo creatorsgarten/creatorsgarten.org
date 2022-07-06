538439109804d18a7f482335958356e6c44bcfb1
chayapatr <chayapatr.arc@gmail.com>
2022-07-07 04:34:48 +0700

    feat: add (alpha) webring & revert sveltekit back to 1.0.0-next.358

--- src/Components/Navbar.svelte
+++ src/Components/Navbar.svelte
@@ -2,7 +2,7 @@
 <section class="align-center flex h-16 mb-6 justify-between pt-6">
   <a href="/"><img src="/images/creatorsgarten.svg" class="h-full" alt="" /></a>
   <div class="flex">
-    <!-- <a href="/ring"><h2 class="hover:text-gray-700 transition-all">Ring</h2></a> -->
+    <a href="/ring"><h2 class="text-black transition-all hover:text-gray-700">Ring</h2></a>
     <a href="/manifesto"><h2 class="text-black transition-all hover:text-gray-700">Manifesto</h2></a
     >
   </div>
