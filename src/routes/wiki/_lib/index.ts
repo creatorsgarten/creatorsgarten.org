47d2e77cc05129b47973d1cd038ba802220272d9
Thai Pangsakulyanont <dtinth@spacet.me>
2022-07-07 11:46:43 -0700

    feat: source wiki data from wiki repo and implement editing (#4)

--- src/routes/wiki/_lib/index.ts
+++ src/routes/wiki/_lib/index.ts
@@ -1,8 +1,44 @@
 import fs from 'fs';
+import axios from 'axios';
+
+interface WikiStorage {
+  mode: 'local' | 'remote' | 'preview';
+  read(slug: string): Promise<string>;
+}
+
+class LocalWikiStorage implements WikiStorage {
+  mode = 'local' as const;
+  async read(slug: string) {
+    return fs.readFileSync(`wiki/${slug}.md`, 'utf8');
+  }
+}
+class RemoteWikiStorage implements WikiStorage {
+  mode = 'remote' as const;
+  constructor(public urlBase: string) {}
+  async read(slug: string) {
+    const response = await axios.get(this.urlBase + slug + '.md');
+    console.log(response.config.url);
+    return Buffer.from(response.data.content, 'base64').toString();
+  }
+}
+
+let storage: WikiStorage | undefined;
+
+function getStorage(): WikiStorage {
+  if (!storage) {
+    const url =
+      process.env.WIKI_STORAGE_URL ||
+      (!fs.existsSync('wiki')
+        ? 'https://directcommit-production.up.railway.app/api/mountpoints/creatorsgarten-wiki/contents/'
+        : '');
+    storage = url ? new RemoteWikiStorage(url) : new LocalWikiStorage();
+  }
+  return storage;
+}
 
 export async function getPage(slug: string): Promise<PageData> {
   return {
-    content: fs.readFileSync(`wiki/${slug}.md`, 'utf8'),
+    content: await getStorage().read(slug),
     mode: 'local'
   };
 }
