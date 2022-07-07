316d0cac009f3773079ff08cfbcf655fbbb1ee45
dtinth on MBP M1 <dtinth@spacet.me>
2022-07-08 00:53:35 +0700

    Expose error message

--- src/routes/wiki/[...slug].svelte
+++ src/routes/wiki/[...slug].svelte
@@ -2,6 +2,7 @@
   import { processMarkdown } from '../../markdown';
   import type { PageData } from './_lib';
 
+  let edit = false;
   export let slug: string;
   export let data: PageData;
 
@@ -11,7 +12,7 @@
 </script>
 
 <div class="cg-container">
-  slug: {JSON.stringify(slug)}
+  <h1 class="text-3xl font-bold mt-16">{slug}</h1>
   <article class="prose prose-lg max-w-none">
     {@html output.html}
   </article>
