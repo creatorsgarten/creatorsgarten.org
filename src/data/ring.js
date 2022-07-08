759cb9ceeff6abad720ecbcee85ca5166ccc2c8c
dtinth on MBP M1 <dtinth@spacet.me>
2022-07-08 08:53:49 +0700

    Prettier

--- src/data/ring.js
+++ src/data/ring.js
@@ -1,26 +1,29 @@
 export const groups = [
-    {
-        name: "Creatorsgarten",
-        link: "https://creatorsgarten.org"
-    },{
-        name: "showdown.space",
-        link: "https://showdown.space",
-    },{
-        name: "Stupid Hackathon Thailand",
-        link: "https://stupidhackth.github.io",
-    }, {
-        name: "Polaryz Club",
-        link: "https://www.instagram.com/polaryz.club/"
-    }
-]
+  {
+    name: 'Creatorsgarten',
+    link: 'https://creatorsgarten.org'
+  },
+  {
+    name: 'showdown.space',
+    link: 'https://showdown.space'
+  },
+  {
+    name: 'Stupid Hackathon Thailand',
+    link: 'https://stupidhackth.github.io'
+  },
+  {
+    name: 'Polaryz Club',
+    link: 'https://www.instagram.com/polaryz.club/'
+  }
+];
 
 export const creators = [
-    {
-        name: "Chayapatr Archiwaranguprok",
-        link: "https://spaceth.co/author/chayapatr"
-    },
-    {
-        name: "Phoomparin Mano",
-        link: "https://poom.dev"
-    }
-]
+  {
+    name: 'Chayapatr Archiwaranguprok',
+    link: 'https://spaceth.co/author/chayapatr'
+  },
+  {
+    name: 'Phoomparin Mano',
+    link: 'https://poom.dev'
+  }
+];
