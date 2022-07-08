5cee4daf1b49fad3ff34087bd203983fe5d40f0c
dtinth on MBP M1 <dtinth@spacet.me>
2022-07-08 22:56:03 +0700

    Constrain width of home page, like previous design

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,29 +13,31 @@
 
   <div class="prose prose-lg max-w-none mt-8">
     <h2>Who are we?</h2>
-    <p>
-      We are a diverse group of people who are united by our love for creating things. We believe
-      that through the lens of creation and technology, we can discover the fun and beauty in
-      everything around us. We stand for:
-    </p>
-    <p>
-      <strong>Creativity</strong>: We believe that creativity is a fundamental human quality that
-      should be nurtured and encouraged. We believe that everyone has the potential to be creative,
-      and that the world is a better place when we all express our creativity.
-    </p>
-    <p>
-      <strong>Empathy</strong>: We believe in putting ourselves in others' shoes, and using our
-      skills to make a positive impact on the world.
-    </p>
-    <p>
-      <strong>Fun</strong>: We believe that work should be enjoyable, and that every day is an
-      opportunity to learn something new and have some fun.
-    </p>
-    <p class="not-prose">
-      <button class=" rounded-md border-2 border-black px-2 py-1 hover:bg-gray-200"
-        ><a href="/manifesto" class="text-black hover:text-black">Manifesto</a></button
-      >
-    </p>
+    <div class="lg:w-11/12">
+      <p>
+        We are a diverse group of people who are united by our love for creating things. We believe
+        that through the lens of creation and technology, we can discover the fun and beauty in
+        everything around us. We stand for:
+      </p>
+      <p>
+        <strong>Creativity</strong>: We believe that creativity is a fundamental human quality that
+        should be nurtured and encouraged. We believe that everyone has the potential to be
+        creative, and that the world is a better place when we all express our creativity.
+      </p>
+      <p>
+        <strong>Empathy</strong>: We believe in putting ourselves in others' shoes, and using our
+        skills to make a positive impact on the world.
+      </p>
+      <p>
+        <strong>Fun</strong>: We believe that work should be enjoyable, and that every day is an
+        opportunity to learn something new and have some fun.
+      </p>
+      <p class="not-prose">
+        <button class=" rounded-md border-2 border-black px-2 py-1 hover:bg-gray-200"
+          ><a href="/manifesto" class="text-black hover:text-black">Manifesto</a></button
+        >
+      </p>
+    </div>
 
     <h2>Hacks</h2>
     <section class="not-prose grid grid-cols-2 gap-2 pb-6 md:grid-cols-3 lg:gap-4">
