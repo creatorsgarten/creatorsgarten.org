6930d74bc60b8020585d3fb9041fe11aaa55f2d4
dtinth on MBP M1 <dtinth@spacet.me>
2022-07-08 22:09:54 +0700

    Use Tailwind Typography plugin for consistent styling of prose

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,48 +2,54 @@
   import { hacks } from '../data/hacks.js';
 </script>
 
+<svelte:head>
+  <title>Creatorsgarten</title>
+</svelte:head>
+
 <div class="cg-container">
   <section>
     <img src="images/cover.webp" class="border-2 border-black" alt="" />
   </section>
 
-  <section class="py-4 lg:w-11/12">
-    <h1 class="py-4 text-3xl font-medium">Who are we?</h1>
-    <p class="pb-2">
+  <div class="prose prose-lg max-w-none mt-8">
+    <h2>Who are we?</h2>
+    <p>
       We are a diverse group of people who are united by our love for creating things. We believe
       that through the lens of creation and technology, we can discover the fun and beauty in
       everything around us. We stand for:
     </p>
-    <p class="pb-2">
-      <span class="font-medium">Creativity</span>: We believe that creativity is a fundamental human
-      quality that should be nurtured and encouraged. We believe that everyone has the potential to
-      be creative, and that the world is a better place when we all express our creativity.
+    <p>
+      <strong>Creativity</strong>: We believe that creativity is a fundamental human quality that
+      should be nurtured and encouraged. We believe that everyone has the potential to be creative,
+      and that the world is a better place when we all express our creativity.
     </p>
-    <p class="pb-2">
-      <span class="font-medium">Empathy</span>: We believe in putting ourselves in others' shoes,
-      and using our skills to make a positive impact on the world.
+    <p>
+      <strong>Empathy</strong>: We believe in putting ourselves in others' shoes, and using our
+      skills to make a positive impact on the world.
     </p>
-    <p class="pb-4">
-      <span class="font-medium">Fun</span>: We believe that work should be enjoyable, and that every
-      day is an opportunity to learn something new and have some fun.
+    <p>
+      <strong>Fun</strong>: We believe that work should be enjoyable, and that every day is an
+      opportunity to learn something new and have some fun.
     </p>
-    <button class=" rounded-md border-2 border-black px-2 py-1 hover:bg-gray-200"
-      ><a href="/manifesto" class="text-black hover:text-black">Manifesto</a></button
-    >
-  </section>
+    <p class="not-prose">
+      <button class=" rounded-md border-2 border-black px-2 py-1 hover:bg-gray-200"
+        ><a href="/manifesto" class="text-black hover:text-black">Manifesto</a></button
+      >
+    </p>
 
-  <section><h1 class="py-4 text-3xl font-medium">Hacks</h1></section>
-  <section class="grid grid-cols-2 gap-2 pb-6 md:grid-cols-3 lg:gap-4">
-    {#each [...hacks].reverse() as hack}
-      <a href={hack.slug.includes('https://') ? hack.slug : 'hacks/' + hack.slug}>
-        <div
-          class="aspect-square w-full border-2 border-black hover:scale-[1.003] hover:shadow-sm hover:transition-all"
-        >
-          <img src={'images/hacks/compressed/' + hack.slug + '.webp'} class="" alt="" />
-        </div>
-      </a>
-    {/each}
-  </section>
+    <h2>Hacks</h2>
+    <section class="not-prose grid grid-cols-2 gap-2 pb-6 md:grid-cols-3 lg:gap-4">
+      {#each [...hacks].reverse() as hack}
+        <a href={hack.slug.includes('https://') ? hack.slug : 'hacks/' + hack.slug}>
+          <div
+            class="aspect-square w-full border-2 border-black hover:scale-[1.003] hover:shadow-sm hover:transition-all"
+          >
+            <img src={'images/hacks/compressed/' + hack.slug + '.webp'} class="" alt="" />
+          </div>
+        </a>
+      {/each}
+    </section>
+  </div>
 </div>
 
 <style>
--- src/routes/manifesto.svelte
+++ src/routes/manifesto.svelte
@@ -1,48 +1,47 @@
-<section class="pb-2 cg-container">
-  <h1 class="pb-4 text-2xl font-medium md:text-4xl">Who are we?</h1>
-  <p>
-    <span class="font-medium">We</span> are a diverse group of people who are united by our love for
-    creating things. We believe that through the lens of creation and technology, we can discover the
-    fun and beauty in everything around us.
-  </p>
-  <p>
-    <span class="font-medium">We</span> don't believe in disciplinary boundaries. As we think that all
-    knowledge is connected, and that by breaking down barriers we can learn more and create more interesting
-    things.
-  </p>
-  <p>
-    <span class="font-medium">We</span> connect people through events, meetups, hackathons, and much
-    more. We strive to create an environment where creators can come together to collaborate, share ideas,
-    and learn from each other.
-  </p>
-  <p>We stand for:</p>
-  <p>
-    <span class="font-medium">Creativity</span>: We believe that creativity is a fundamental human
-    quality that should be nurtured and encouraged. We believe that everyone has the potential to be
-    creative, and that the world is a better place when we all express our creativity.
-  </p>
-  <p>
-    <span class="font-medium">Empathy</span>: We believe in putting ourselves in others' shoes, and
-    using our skills to make a positive impact on the world.
-  </p>
-  <p>
-    <span class="font-medium">Fun</span>: We believe that work should be enjoyable, and that every
-    day is an opportunity to learn something new and have some fun.
-  </p>
-  <p>
-    The name <span class="font-medium">Creatorsgarten</span> comes from the word creators and garten,
-    as in Kindergarten, which means garden of children in German. We chose this name because we want
-    to create a garden for all creators where they can come to create, learn, and share.
-  </p>
-  <p>
-    We invite you to join us on this journey of exploration and creation. Together, let’s create
-    things.
-  </p>
-  <img class="by-4 border-2 border-black" src="images/cover.webp" alt="" />
-</section>
+<svelte:head>
+  <title>Manifesto | Creatorsgarten</title>
+</svelte:head>
 
-<style>
-  p {
-    @apply pb-4;
-  }
-</style>
+<div class="cg-container">
+  <article class="prose prose-lg max-w-none">
+    <h2>Who are we?</h2>
+    <p>
+      <strong>We</strong> are a diverse group of people who are united by our love for creating things.
+      We believe that through the lens of creation and technology, we can discover the fun and beauty
+      in everything around us.
+    </p>
+    <p>
+      <strong>We</strong> don't believe in disciplinary boundaries. As we think that all knowledge is
+      connected, and that by breaking down barriers we can learn more and create more interesting things.
+    </p>
+    <p>
+      <strong>We</strong> connect people through events, meetups, hackathons, and much more. We strive
+      to create an environment where creators can come together to collaborate, share ideas, and learn
+      from each other.
+    </p>
+    <p>We stand for:</p>
+    <p>
+      <strong>Creativity</strong>: We believe that creativity is a fundamental human quality that
+      should be nurtured and encouraged. We believe that everyone has the potential to be creative,
+      and that the world is a better place when we all express our creativity.
+    </p>
+    <p>
+      <strong>Empathy</strong>: We believe in putting ourselves in others' shoes, and using our
+      skills to make a positive impact on the world.
+    </p>
+    <p>
+      <strong>Fun</strong>: We believe that work should be enjoyable, and that every day is an
+      opportunity to learn something new and have some fun.
+    </p>
+    <p>
+      The name <strong>Creatorsgarten</strong> comes from the word creators and garten, as in Kindergarten,
+      which means garden of children in German. We chose this name because we want to create a garden
+      for all creators where they can come to create, learn, and share.
+    </p>
+    <p>
+      We invite you to join us on this journey of exploration and creation. Together, let’s create
+      things.
+    </p>
+    <img class="by-4 border-2 border-black" src="images/cover.webp" alt="" />
+  </article>
+</div>
