dbc831abddfb3e61ddf912438fe5542a10037c44
dtinth on MBP M1 <dtinth@spacet.me>
2022-09-03 01:48:39 +0700

    Add ideas board

---
+++ src/routes/ideas/[number]/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+  export let data: PageData;
+</script>
+
+<svelte:head>
+  <title>Event Idea: {data.idea.title} | Creatorsgarten</title>
+</svelte:head>
+
+<div class="cg-container min-h-[80vh]">
+  <div class="prose max-w-none">
+    <div class="mt-14">
+      <a href="/ideas" class="text-blue-500 hover:text-blue-600">← Back to Event Ideas</a>
+    </div>
+    <h2 class="text-3xl md:text-4xl font-semibold mt-2">{data.idea.title}</h2>
+    {@html data.idea.bodyHTML}
+    <p class="not-prose">
+      <a href={data.idea.url} class="cg-btn">Discuss on GitHub</a>
+    </p>
+  </div>
+</div>
