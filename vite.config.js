8e5c5b077105f38a303043c9bb8d60fc85325785
dtinth on MBP M1 <dtinth@spacet.me>
2022-09-03 01:52:42 +0700

    ugh

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
   plugins: [sveltekit()],
   ssr: {
-    noExternal: ['p-memoize']
+    noExternal: ['p-memoize', 'devalue']
   }
 };
 
