d22cdbd1045e58537a6c450e16d04b0a180cc1c5
dtinth on MBP M1 <dtinth@spacet.me>
2022-09-04 01:50:41 +0700

    Upd navbar

--- src/Components/Navbar.svelte
+++ src/Components/Navbar.svelte
@@ -1,11 +1,13 @@
-<section class="cg-large-container items-center flex h-14 mt-1 pt-4 pb-2 justify-between">
-  <a href="/" class="relative h-full flex items-center"
+<section class="cg-large-container mt-1 flex h-14 items-center justify-between pt-4 pb-2">
+  <a href="/" class="relative flex h-full items-center"
     ><img src="/images/creatorsgarten.svg" class="h-full" alt="" />
     <!-- <h4 class="hidden md:block text-black pl-2 font-semibold text-xl">Creatorsgarten</h4> -->
   </a>
   <div class="flex">
+    <a href="/events"><p class="text-black transition-all hover:text-gray-700">Events</p></a>
     <a href="/wiki/About/Wiki"><p class="text-black transition-all hover:text-gray-700">Wiki</p></a>
     <a href="/ring"><p class="text-black transition-all hover:text-gray-700">Ring</p></a>
+    <a href="/ideas"><p class="text-black transition-all hover:text-gray-700">Ideas</p></a>
     <a href="/wiki/Manifesto"
       ><p class="text-black transition-all hover:text-gray-700">Manifesto</p></a
     >
@@ -14,6 +16,6 @@
 
 <style>
   p {
-    @apply ml-3 lg:pl-4 font-normal text-lg;
+    @apply ml-3 text-lg font-normal lg:pl-4;
   }
 </style>
