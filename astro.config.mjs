753b144b1ab538c2d309974991f167fab05612ab
Phumrapee Limpianchop <git@rayriffy.com>
2023-04-27 00:01:03 +0900

    chore: style config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from 'astro/config'
+
+/* Adapter */
 import node from '@astrojs/node'
+
+/* Integrations */
 import tailwind from '@astrojs/tailwind'
 import image from '@astrojs/image'
 import mdx from '@astrojs/mdx'
 import prefetch from '@astrojs/prefetch'
 import compress from 'astro-compress'
+
+/* Markdown plugins */
+import { headerPlugin } from './src/plugins/headerPlugin.mjs'
 import externalLinks from 'remark-external-links'
 import sectionize from 'remark-sectionize'
-import { headerPlugin } from './src/plugins/headerPlugin.mjs'
 
 // https://astro.build/config
 import react from '@astrojs/react'
