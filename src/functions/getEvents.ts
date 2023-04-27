c75b61cef50f4abf9cbc6ada20113e115dc9c9dc
Phumrapee Limpianchop <git@rayriffy.com>
2023-04-28 01:52:32 +0900

    chore: staged changes

---
+++ src/functions/getEvents.ts
@@ -0,0 +1,40 @@
+import dayjs from "dayjs"
+
+import { contentsgarten } from "$constants/contentsgarten"
+
+import type { EventFrontmatter } from "$types/EventFrontmatter"
+import { readFileSystem, writeFileSystem } from "./fileSystem"
+
+interface Event {
+  id: string;
+  name: string;
+  date: dayjs.Dayjs;
+}
+
+export const getEvents = async () => {
+  const cachedEvents = await readFileSystem<Event[]>(['events'])
+
+  if (cachedEvents !== null)
+    return cachedEvents.data.map(o => ({ ...o, date: dayjs(o.date) }))
+
+  const fetchedEvents: Event[] = (
+    await contentsgarten.search.query({
+      prefix: 'Events/',
+    })
+  ).results
+    .map(event => {
+      const frontmatter = event.frontMatter as EventFrontmatter
+      return {
+        id: event.pageRef.replace('Events/', ''),
+        name: frontmatter.name,
+        date: dayjs(frontmatter.date),
+      }
+    })
+    .sort((a, b) => {
+      return b!.date.unix() - a!.date.unix()
+    })
+
+  await writeFileSystem(['events'], fetchedEvents)
+
+  return fetchedEvents
+}
