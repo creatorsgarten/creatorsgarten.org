f17806f1d5453af3aaab2f45ea8745d4637b3e7c
Thai Pangsakulyanont <dtinth@spacet.me>
2023-04-28 17:45:58 +0000

    oauth state (#75)
    When redirect, need to add some redirection hint to `state=` parameter,
    so `oauth_gateway` knows how to redirect back to the app correctly.
    
    - `state` begins with `new!` &rarr; `https://new.creatorsgarten.org`
    - `state` begins with `localhost3000!` &rarr; `http://localhost:3000`
    
    See implementation here
    https://github.com/dtinth/oauth_gateway/blob/main/eventpop_callback.html
    
    Co-authored-by: Phumrapee Limpianchop <git@rayriffy.com>

--- src/pages/auth/login.ts
+++ src/pages/auth/login.ts
@@ -2,8 +2,10 @@ import CSRF from 'csrf'
 
 import type { APIRoute } from 'astro'
 
-export const get: APIRoute = async ({ redirect }) => {
+export const get: APIRoute = async ({ request, redirect }) => {
   const csrfInstance = new CSRF()
+  const redirectHint =
+    new URL(request.url).hostname === 'localhost' ? 'localhost3000' : 'new'
   const csrfToken = csrfInstance.create(import.meta.env.CSRF_SECRET ?? 'demodash')
 
   const loginURI = `https://www.eventpop.me/oauth/authorize?${new URLSearchParams(
@@ -12,7 +14,7 @@ export const get: APIRoute = async ({ redirect }) => {
       redirect_uri:
         'https://dtinth.github.io/oauth_gateway/eventpop_callback.html',
       response_type: 'code',
-      state: import.meta.env.PROD ? `eventpop-${csrfToken}` : 'eventpop-local'
+      state: `${redirectHint}!eventpop-${csrfToken}`
     }
   ).toString()}`
 
