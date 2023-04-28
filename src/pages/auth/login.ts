59d9467969243f009ec04058f452e92b4c530a3b
dtinth on MBP M1 <dtinth@spacet.me>
2023-04-29 00:20:19 +0700

    go

--- src/pages/auth/login.ts
+++ src/pages/auth/login.ts
@@ -2,17 +2,18 @@ import CSRF from 'csrf'
 
 import type { APIRoute } from 'astro'
 
-export const get: APIRoute = async ({ redirect }) => {
+export const get: APIRoute = async ({ request, redirect }) => {
   const csrfInstance = new CSRF()
   const csrfToken = csrfInstance.create(process.env.CSRF_SECRET ?? '')
-
+  const redirectHint =
+    new URL(request.url).hostname === 'localhost' ? 'localhost3000' : 'new'
   const loginURI = `https://www.eventpop.me/oauth/authorize?${new URLSearchParams(
     {
       client_id: import.meta.env.EVENTPOP_CLIENT_ID ?? '',
       redirect_uri:
         'https://dtinth.github.io/oauth_gateway/eventpop_callback.html',
       response_type: 'code',
-      state: import.meta.env.PROD ? csrfToken : 'local'
+      state: redirectHint + '!' + (import.meta.env.PROD ? csrfToken : 'local'),
     }
   ).toString()}`
 
