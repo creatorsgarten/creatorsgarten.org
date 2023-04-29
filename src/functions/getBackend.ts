c9864037bc07b8aefee78c2182733b67d8cd3418
dtinth on MBP M1 <dtinth@spacet.me>
2023-04-30 00:03:48 +0700

    extract localLink to its own file

--- src/functions/getBackend.ts
+++ src/functions/getBackend.ts
@@ -1,27 +1,8 @@
 import { createTRPCProxyClient, httpLink } from '@trpc/client'
-import type { AnyRouter, inferRouterContext } from '@trpc/server'
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 import type { AstroGlobal } from 'astro'
 import { getAuthTokenFromAstro } from './auth/getAuthTokenFromAstro'
 import { AppRouter, appRouter } from 'src/backend'
-
-function localLink<TRouter extends AnyRouter>(
-  router: TRouter,
-  context: inferRouterContext<TRouter>
-) {
-  return httpLink({
-    url: 'http://local',
-    fetch: async (...args) => {
-      const request = new Request(...args)
-      return fetchRequestHandler({
-        endpoint: '',
-        req: request,
-        router,
-        createContext: () => context,
-      })
-    },
-  })
-}
+import { localLink } from './localLink'
 
 export function getBackend(Astro: Pick<AstroGlobal, 'cookies'>) {
   const token = getAuthTokenFromAstro(Astro)
