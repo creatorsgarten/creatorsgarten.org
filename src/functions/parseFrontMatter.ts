750250711abd22e0481a4aa22aef8a0bc54e4bdd
dtinth on MBP M1 <dtinth@spacet.me>
2023-04-30 20:58:30 +0700

    add frontmatter validation

---
+++ src/functions/parseFrontMatter.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod'
+
+const frontMatterSchema = z.object({
+  event: z
+    .object({
+      name: z.string(),
+      location: z.string().optional(),
+      date: z.string(),
+      endDate: z.string().optional(),
+      hosts: z.array(z.string()).default([]),
+      site: z.string().optional(),
+      eventpopId: z.coerce.number().optional(),
+    })
+    .optional(),
+})
+
+export function parseFrontMatter(data: any) {
+  return frontMatterSchema.safeParse(data)
+}
