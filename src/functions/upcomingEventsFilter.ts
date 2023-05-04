7163ed2e5e4d34d08a074c8ee104176f74d2f42b
Phumrapee Limpianchop <git@rayriffy.com>
2023-05-05 07:06:23 +0900

    fix: filter

--- src/functions/upcomingEventsFilter.ts
+++ src/functions/upcomingEventsFilter.ts
@@ -2,13 +2,13 @@ import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone'
 
-import type { Event } from './getEvents'
-
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
+import type { Event } from './getEvents'
 export const upcomingEventsFilter = (event: Event) => {
   const targetToCompare = event.endDate ?? event.date
 
-  return targetToCompare.tz('Asia/Bangkok').endOf('day').isAfter(new Date())
+
+  return dayjs(targetToCompare).tz('Asia/Bangkok').endOf('day').isAfter(new Date())
 }
