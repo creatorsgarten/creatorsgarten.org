6bb6621de194a7f377a47c7d84a00efee645f888
dtinth on MBP M1 <dtinth@spacet.me>
2023-05-11 21:07:08 +0700

    add pseudo random number generator

---
+++ src/functions/prng.ts
@@ -0,0 +1,19 @@
+import type { AstroGlobal } from 'astro'
+import { createHash, randomUUID } from 'crypto'
+
+const uidMap = new WeakMap<any, string>()
+
+export function prng(
+  astro: Pick<AstroGlobal, 'request'>,
+  key: string | string[]
+) {
+  let uid = uidMap.get(astro.request)
+  if (!uid) {
+    uid = randomUUID()
+    uidMap.set(astro, uid)
+  }
+  const h = createHash('md5')
+  h.update(uid)
+  h.update(JSON.stringify(key))
+  return parseInt(h.digest('hex').slice(0, 8), 16) / 0x100000000
+}
