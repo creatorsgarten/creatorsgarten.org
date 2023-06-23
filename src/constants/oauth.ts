b294253585d486a96330c94bcda6d7f2fe05bd86
Thai Pangsakulyanont <dtinth@spacet.me>
2023-06-24 03:41:42 +0700

    update trust

--- src/constants/oauth.ts
+++ src/constants/oauth.ts
@@ -7,6 +7,13 @@ export const oauthClients: OAuthClientConfig[] = [
       'https://authgarten-example.vercel.app/api/auth/callback/creatorsgarten',
     ],
   },
+  {
+    clientId: 'https://github.com/StupidHackTH/event-currency-interface',
+    name: 'wallet.7th.stupid.hackathon.in.th',
+    redirectUris: [
+      'https://wallet.7th.stupid.hackathon.in.th/api/auth/callback/creatorsgarten',
+    ],
+  },
 ]
 
 export interface OAuthClientConfig {
