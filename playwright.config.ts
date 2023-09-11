210aeb2b96e9ede4afa933bb3768dd3ce83d6a92
Phumrapee Limpianchop <git@rayriffy.com>
2023-09-11 20:12:29 +0900

    refactor: change playwright test port

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:4321',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: process.env.CI ? 'on' : 'on-first-retry',
@@ -104,7 +104,7 @@ const config: PlaywrightTestConfig = {
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm dev',
-    port: 3000,
+    port: 4321,
     reuseExistingServer: true,
   },
 }
