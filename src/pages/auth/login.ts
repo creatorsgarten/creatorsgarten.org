ac00bb69ba37a0a3309fb4603d84c6fbe2fa3bdb
Phumrapee Limpianchop <git@rayriffy.com>
2023-09-16 19:41:03 +0900

    refactor: dynamic redirect hint

--- src/pages/auth/login.ts
+++ src/pages/auth/login.ts
@@ -10,7 +10,7 @@ export const get: APIRoute = async ({ request, redirect, url }) => {
 
   const csrfInstance = new CSRF()
   const redirectHint =
-    new URL(request.url).hostname === 'localhost' ? 'localhost3000' : 'new'
+    url.hostname === 'localhost' ? `localhost${url.port}` : 'new'
   const csrfToken = csrfInstance.create(csrfSecret ?? 'demodash')
 
   const loginURI = `https://www.eventpop.me/oauth/authorize?${new URLSearchParams(
