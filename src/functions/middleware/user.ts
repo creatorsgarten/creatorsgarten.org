3b52a10d7e11af1f475ee2bc81017c578102dab3
Phumrapee Limpianchop <git@rayriffy.com>
2023-09-16 19:57:34 +0900

    feat: memorize user data

--- src/functions/middleware/user.ts
+++ src/functions/middleware/user.ts
@@ -1,7 +1,13 @@
 import { defineMiddleware } from 'astro/middleware'
+import mem from 'mem'
 
 export const userMiddleware = defineMiddleware(async ({ locals }, next) => {
-  locals.user = await locals.backend.auth.getAuthenticatedUser.query()
+  locals.user = await mem(
+    () => locals.backend.auth.getAuthenticatedUser.query(),
+    {
+      maxAge: 1000 * 60 * 15, // 15 minutes
+    }
+  )()
 
   return next()
 })
