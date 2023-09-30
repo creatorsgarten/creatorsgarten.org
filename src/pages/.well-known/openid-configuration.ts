f403f37f5da29ffbfc00811aa40fc354f9847c14
Thai Pangsakulyanont <dtinth@spacet.me>
2023-09-30 10:15:15 +0000

    Update openid-configuration.ts

--- src/pages/.well-known/openid-configuration.ts
+++ src/pages/.well-known/openid-configuration.ts
@@ -3,19 +3,18 @@ import { getBackend } from '$functions/getBackend'
 
 export const get: APIRoute = async Astro => {
   const base = Astro.url.origin
-
-  return {
+  const body = JSON.stringify({
+    id_token_signing_alg_values_supported: ['RS256'],
+    issuer: 'https://creatorsgarten.org',
+    jwks_uri: `${base}/.well-known/jwks`,
+    authorization_endpoint: `${base}/auth/authorize`,
+    response_types_supported: ['id_token'],
+    subject_types_supported: ['public', 'pairwise'],
+  })
+  return new Response(body, {
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
     },
-    body: JSON.stringify({
-      id_token_signing_alg_values_supported: ['RS256'],
-      issuer: 'https://creatorsgarten.org',
-      jwks_uri: `${base}/.well-known/jwks`,
-      authorization_endpoint: `${base}/auth/authorize`,
-      response_types_supported: ['id_token'],
-      subject_types_supported: ['public', 'pairwise'],
-    }),
-  }
+  })
 }
