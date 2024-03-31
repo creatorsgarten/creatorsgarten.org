49d8e2a6bce732a2bc9b6c64e77d2de45bce79e1
Phumrapee Limpianchop <git@rayriffy.com>
2024-03-31 23:05:27 +0900

    refactor: delete and ignore .svelte-kit

--- .svelte-kit/generated/client/nodes/5.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/ideas/[number]/+page.svelte";
--- .svelte-kit/generated/client/nodes/7.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/wiki/[...slug]/+page.svelte";
