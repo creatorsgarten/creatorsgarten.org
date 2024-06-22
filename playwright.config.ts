b22b416e1ab0d1f27fece6d3205bb15178ee5885
Phumrapee Limpianchop <git@rayriffy.com>
2024-06-23 00:38:35 +0900

    fix: e2e dynamic ports

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,8 @@
 import type { PlaywrightTestConfig } from '@playwright/test'
 import { devices } from '@playwright/test'
 
+const targetedPort = process.env.PORT ? Number(process.env.PORT) : 3000
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -36,7 +38,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:' + targetedPort,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: process.env.CI ? 'on' : 'on-first-retry',
@@ -104,7 +106,7 @@ const config: PlaywrightTestConfig = {
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm dev',
-    port: 3000,
+    port: targetedPort,
     reuseExistingServer: true,
   },
 }
